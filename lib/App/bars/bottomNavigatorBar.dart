7ef7943f63dfd749c1e91e699b27ac174898d9e7
André Luiz H. Kanashiro <alhkanashiro@gmail.com>
2022-05-18 12:18:54 -0300

    Ajustando BottomNavigatorBar

--- lib/App/bars/bottomNavigatorBar.dart
+++ lib/App/bars/bottomNavigatorBar.dart
@@ -15,58 +15,64 @@ class bottomNavigatorBarScreen extends StatefulWidget {
 }
 
 class _bottomNavigatorBarState extends State<bottomNavigatorBarScreen> {
-  int _selectedIndex = 1;
+  int _selectedIndex = 0;
 
   Widget build(BuildContext context) {
     const double _iconSize = 20;
     const double _iconSizeSelected = 23;
 
     return BottomNavigationBar(
-      type: BottomNavigationBarType.fixed,
-      backgroundColor: Color(0xFFaefb2a),
-      items: <BottomNavigationBarItem>[
+      type: BottomNavigationBarType.shifting,
+      backgroundColor: Colors.black,
+      items: [
         BottomNavigationBarItem(
-            icon: IconButton(
-              icon: _selectedIndex == 1
-                  ? Images.selectedStories
-                  : Images.selectedStories,
-              onPressed: () {
-                Navigator.pushNamed(context, '/home');
-              },
-              iconSize: _selectedIndex == 1 ? _iconSizeSelected : _iconSize,
-            ),
-            label: 'Tarefas'),
+          icon: IconButton(
+            icon: _selectedIndex == 0
+                ? Images.selectedStories
+                : Images.selectedStories,
+            onPressed: () {
+              Navigator.pushNamed(context, '/home');
+            },
+            iconSize: _selectedIndex == 0 ? _iconSizeSelected : _iconSize,
+          ),
+          label: 'Tarefas',
+        ),
         BottomNavigationBarItem(
-            icon: IconButton(
-              icon: _selectedIndex == 1
-                  ? Images.selectedRanking
-                  : Images.selectedRanking,
-              onPressed: () {
-                Navigator.pushNamed(context, '/dashboard');
-              },
-              iconSize: _selectedIndex == 1 ? _iconSizeSelected : _iconSize,
-            ),
-            label: 'Dashboard'),
+          icon: IconButton(
+            icon: _selectedIndex == 1
+                ? Images.selectedRanking
+                : Images.selectedRanking,
+            onPressed: () {
+              Navigator.pushNamed(context, '/dashboard');
+            },
+            iconSize: _selectedIndex == 1 ? _iconSizeSelected : _iconSize,
+          ),
+          label: 'Dashboard',
+        ),
         BottomNavigationBarItem(
-            icon: IconButton(
-              icon: _selectedIndex == 1
-                  ? Images.selectedProfile
-                  : Images.selectedProfile,
-              onPressed: () {
-                Navigator.pushNamed(context, '/profile');
-              },
-              iconSize: _selectedIndex == 1 ? _iconSizeSelected : _iconSize,
-            ),
-            label: 'Perfil'),
+          icon: IconButton(
+            icon: _selectedIndex == 2
+                ? Images.selectedProfile
+                : Images.selectedProfile,
+            onPressed: () {
+              Navigator.pushNamed(context, '/profile');
+            },
+            iconSize: _selectedIndex == 2 ? _iconSizeSelected : _iconSize,
+          ),
+          label: 'Perfil',
+        ),
       ],
       currentIndex: _selectedIndex,
-      onTap: _onItemTapped,
+      selectedItemColor: Colors.black,
+      unselectedItemColor: Colors.blue,
+      showUnselectedLabels: true,
+      elevation: 0,
+      onTap: _onTap,
     );
   }
 
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
+  void _onTap(int index) {
+    _selectedIndex = index;
+    setState(() {});
   }
 }
