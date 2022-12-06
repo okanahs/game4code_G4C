a97765e6e49bc2da1b8862990e2926aaf0036189
André Kanashiro <alhkanashiro@gmail.com>
2022-12-06 11:06:43 -0300

    ahh

--- frontend_app/android/app/src/main/kotlin/com/example/game4code_front/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.game4code_front
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- frontend_app/lib/App/Utils/Content.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-
-class Content {
-  final String? text;
-  final Color? color;
-
-  Content({this.text, this.color});
-}
